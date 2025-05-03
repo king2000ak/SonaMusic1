ac60da898477626c0aa30fb3bf1b67cfe08de5ac
Zinda Lash <71366520+king2000ak@users.noreply.github.com>
2025-05-03 15:10:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GayatrixAnand/SonaMusic1",
+    "https://github.com/king2000ak/SonaMusic1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
